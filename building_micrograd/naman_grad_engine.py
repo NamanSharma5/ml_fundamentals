93fdbcd6851474d54a5cdc96ae0b8d2e3c6df900
naman <naman.angrish@yahoo.co.uk>
2024-12-20 10:48:28 +0000

    sync up before travel

--- building_micrograd/naman_grad_engine.py
+++ building_micrograd/naman_grad_engine.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from functools import reduce
 """
 Purpose of this engine is to provide primitives which support automatic differentiation via backpropagation (chain rule).
 
@@ -7,7 +8,7 @@ Some example expressions:
 a = Value(2)
 b = Value(3)
 c = a + b
-d = a * b + c = a * b + a + b 
+d = a * b + c = a * b + a + b
 
 What interface do we want to provide???
 - well the value method should support the basic operations of addition, subtraction, multiplication, and division + the power operator(i.e. repeated multiplication)
@@ -17,13 +18,22 @@ What interface do we want to provide???
 
 """
 Order of implmentation:
-1) Value class 
+1) Value class
 - attributes to support forward pass (data, operation, children)
 
-"""
+ML Learnings:
+ - its a graph, not a tree!! (can have multiple parents)
+ i.e. V(3) = V(2) + V(1)  ; V(4) = V(3) + V(1) ; V(4).backward()
 
+1) Leaf Value nodes can feed into the computation graph in multiple operations, so need to be able to accumulate gradients from multiple sources
+- otherwise need to change when graph is being constructed with operations to create a new value object if we are likely to reuse a node
+
+SWE Learnings:
+1) Keep backward on operation on operation method rather than backward function
+
+"""
 class Operation(Enum):
-    # ID? 
+    # ID?
     ADD = 1
     SUB = 2
     MUL = 3
@@ -33,15 +43,16 @@ class Operation(Enum):
 class Value:
     def __init__(self, data):
         self.data = data
-        # this gradient is the contribution of this self node to the gradient of the node where we called backward (note if this is not t)
-        self.grad = 0 
+        # this gradient is the contribution of this self node to the gradient of the node where we called backward
+        # to implement w.r.p simplest thing is just to search for the w.r.p node in graph
+        self.grad = 0
         self.operation:Optional[Enum] = None
-        self._children:list[Value] = []
-        self._backward_fn = None
+        self._children:set[Value] = set()
+        self._backward_fn = lambda: None # base case for leaf nodes in no graph
 
     def __repr__(self):
         return f"Value({self.data}) with grad {self.grad}"
-    
+
     # NOTE: equality on data attribute does not hold since lineage is not considered
     def __equal__(self, other):
         return self.data == other.data and self.operation == other.operation and self._children == other.children
@@ -50,45 +61,64 @@ class Value:
         other = other if isinstance(other, Value) else Value(other)
         new_value = Value(self.data + other.data)
         new_value.operation = Operation.ADD
-        new_value._children = [self, other]
+        new_value._children = {self, other}
+
+        def _backward():
+            assert new_value.grad is not None, "Gradient is None"
+            for child in new_value._children:
+                # in addition we just pass through the gradient to the children,
+                #   irrespective of the other values in the operation
+                child.grad += new_value.grad
+
+        new_value._backward_fn = _backward
         return new_value
-    
-    def __mul__(self, other): 
+
+    def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         new_value = Value(self.data * other.data)
         new_value.operation = Operation.MUL
-        new_value._children = [self, other]
+        new_value._children = {self, other}
+
+        def _backward():
+            # backward is harder to reason about when you have multiple children
+            total_product = reduce(lambda x, y: x * y, [child.data for child in new_value._children])
+            for child in new_value._children:
+                child: Value
+                remaining_product = total_product / child.data
+                child.grad += new_value.grad * remaining_product
+
+        new_value._backward_fn = _backward
         return new_value
 
     def backward(self, with_respect_to:Optional['Value'] = None, parent_grad:Optional[float] = 1):
-        # compute gradient from node w.r.t. value 
+        # compute gradient from node w.r.t. value
         # NOTE: if backward called a value object, with the with_respect_to_argument as None, then for each child node we consider gradient w.r.t. to that child node
         # search for the node w.r.t. which we are computing the gradient
         # whilst looking for the node take a running product of gradients encountered
         if isinstance(parent_grad, Value):
             print(f"Assuming you made a typo and w.r.p to be {parent_grad}")
             #TODO
-        
-        # only nodes with no children contribute to the gradient
-        if len(self._children) == 0:
-            if with_respect_to is None or with_respect_to == self:
-                self.grad = parent_grad
-            else:
-                self.grad = 0
-            return self.grad
 
-        match self.operation:
-            case Operation.ADD:
-                for child in self._children:
-                    child.backward(with_respect_to=with_respect_to, parent_grad=parent_grad)
+        visited = set()
+        differentiation_order = []
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                differentiation_order.append(v) # if here, its preorder
+                for child in v._children:
+                    build_topo(child)
+                # differentiation_order.append(v) if here, its postorder, hence need to reverse (i.e. differentiate parent before children)
+            return differentiation_order
 
-
-            case Operation.MUL:
-                for index, child in enumerate(self._children):
-                    # assumes two children
-                    other_child = self._children[1 - index]
-                    child_grad = parent_grad * other_child.data
-                    child.backward(with_respect_to=with_respect_to, parent_grad=child_grad)        
-
-            case _: 
-                raise NotImplementedError(f"Operation {self.operation} not implemented")
+        differentiation_order = build_topo(self)
+        self.grad = parent_grad
+        # print(f"Differentiation order: {differentiation_order}")
+        # print(f"Parent grad: {parent_grad}")
+        for node in differentiation_order:
+            node: Value
+            node._backward_fn()
+            if with_respect_to is not None:
+                if node == with_respect_to:
+                    break
+                else:
+                    node.grad = 0
--- building_micrograd/test_grad.py
+++ building_micrograd/test_grad.py
@@ -1,5 +1,6 @@
 import unittest
 from naman_grad_engine import Value
+# from reference_engine import Value
 
 class TestGradients(unittest.TestCase):
 
@@ -27,14 +28,55 @@ class TestGradients(unittest.TestCase):
         self.assertEqual(a.grad, 3, f"Expected gradient of a to be 3, got {a.grad}")
         self.assertEqual(b.grad, 2, f"Expected gradient of b to be 2, got {b.grad}")
 
-    def test_complex_expression(self):
+    def test_multiplication_and_addition(self):
         a = Value(2)
         b = Value(3)
         c = a + b
-        d = a * b + c
+        d = a * c
+
         d.backward()
-        self.assertEqual(a.grad, 4, f"Expected gradient of a to be 4, got {a.grad}")
-        self.assertEqual(b.grad, 3, f"Expected gradient of b to be 3, got {b.grad}")
+        self.assertEqual(b.grad, 2, f"Expected gradient of b to be 2, got {b.grad}")
+        self.assertEqual(a.grad, 7, f"Expected gradient of a to be 5, got {a.grad}")
+
+        
+    def test_complex_expression(self):
+        # So this is a bad example because the nodes in the tree are not unique
+        # meaning that they will have different gradients but the same data
+        # my solution does not accumulate the gradients in the same way as the reference solution
+        a = Value(2)
+        b = Value(3)
+        c = a + b # 5
+        d = a * b # 6
+        e = c + d # 11
+        f = e * e # 121 # (c + d)^2 = c^2 + 2cd + d^2 = 
+        self.assertEqual(f.data, 121, f"Expected f to be 121, got {f.data}")
+        # c.backward(with_respect_to=a)
+        c.backward()
+        self.assertEqual(a.grad, 1, f"Expected gradient of a to be 1, got {a.grad}")
+        # e.backward(with_respect_to=a)
+        e.backward()
+        self.assertEqual(a.grad, b.data + 1, f"Expected gradient of a to be {b.data + 1}, got {a.grad}")
+        # f.backward(with_respect_to=e)
+        # self.assertEqual(e.grad, 2 * e.data, f"Expected gradient of e to be {2 * e.data}, got {e.grad}")
+        # f.backward(with_respect_to=a)
+        # self.assertEqual(a.grad, 2 * a.data + 2 * b.data, f"Expected gradient of a to be {2 * a.data + 2 * b.data}, got {a.grad}")
+
+    def test_chatgpt_suggested(self):
+        a = Value(2)
+        b = Value(3)
+        c = a + b    # 5
+        d = a * b    # 6
+        e = c + d    # 11
+        f = e * e    # 121
+
+        # Forward pass check
+        self.assertEqual(f.data, 121, f"Expected f to be 121, got {f.data}")
+
+        # Backward pass for f
+        f.backward()
+
+        # Correct gradient for a
+        self.assertEqual(a.grad, 88, f"Expected gradient of a to be 88, got {a.grad}")
 
 if __name__ == "__main__":
     unittest.main()
